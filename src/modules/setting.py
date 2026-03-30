9a2f88ca612effd7c9b141924a14dccbc2a587c0
Lupiabz <166263411+Lupiabz@users.noreply.github.com>
2026-03-30 23:02:27 +0900

    機械学習

---
+++ src/modules/setting.py
@@ -0,0 +1,19 @@
+# 絶対パスで指定する．
+BASE_PATH = r"C:\Users\lupia\expC2"
+# 以下は変更しない．
+CHARACTER_PATH = rf"{BASE_PATH}/character"
+OUTPUT_PATH = rf"{BASE_PATH}/output"
+
+aug_folders = [
+    "chara0_aug",
+    "chara1_aug",
+    "chara2_aug",
+    "chara3_aug",
+]
+
+chara_folders = [
+    "chara0",
+    "chara1",
+    "chara2",
+    "chara3",
+]
